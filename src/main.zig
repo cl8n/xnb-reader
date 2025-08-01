5511ea63af3442b5ddc5bacaab848084f77e39d2
clayton <me@clayton.cc>
2025-08-01 02:19:58 -0700

    Initial commit

---
+++ src/main.zig
@@ -0,0 +1,140 @@
+const std = @import("std");
+const xnb_reader = @import("xnb_reader");
+const Xnb = xnb_reader.Xnb;
+const XnbParseError = xnb_reader.XnbParseError;
+
+var program_name: []const u8 = undefined;
+
+pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    var args = try std.process.argsWithAllocator(allocator);
+    defer args.deinit();
+
+    program_name = std.fs.path.basename(args.next() orelse "");
+
+    const in_filename = args.next() orelse "-";
+    const out_filename = args.next() orelse "-";
+
+    if (std.mem.eql(u8, in_filename, "-h") or
+        std.mem.eql(u8, in_filename, "--help") or
+        std.mem.eql(u8, in_filename, "-?"))
+    {
+        printUsage(false);
+        return;
+    }
+
+    const in_file = if (std.mem.eql(u8, in_filename, "-"))
+        std.io.getStdIn()
+    else
+        std.fs.cwd().openFile(in_filename, .{}) catch {
+            std.log.err("Failed to open {s}", .{in_filename});
+            printUsage(true);
+            return;
+        };
+    defer in_file.close();
+
+    var buffered_reader = std.io.bufferedReader(in_file.reader());
+    const reader = buffered_reader.reader();
+
+    const xnb = Xnb.parse(allocator, reader.any()) catch |err| {
+        std.log.err("{s}", .{switch (err) {
+            XnbParseError.CompressedUnsupported => "Compressed XNB files are not supported",
+            XnbParseError.InvalidMagic => "Invalid magic bytes in XNB file",
+            XnbParseError.InvalidTexture2DSize => "Invalid Texture2D size",
+            XnbParseError.InvalidTypeReaderIndex => "Invalid type reader index in XNB file",
+            XnbParseError.SharedResourcesUnsupported => "Shared resources in XNB files are not supported",
+            XnbParseError.UnknownTypeReader => "Unknown type reader in XNB file",
+            else => "Error reading XNB file",
+        }});
+        return;
+    };
+
+    switch (xnb.resource) {
+        .effect => {
+            std.log.err("Effects not supported yet", .{});
+            return;
+        },
+        .texture_2d => |texture| {
+            if (texture.mips.len != 1) {
+                std.log.err("Textures with more than 1 mip not supported yet", .{});
+                return;
+            }
+
+            if (texture.pixel_format != .color) {
+                std.log.err("Textures with pixel format other than RGBA are not supported yet", .{});
+                return;
+            }
+
+            print("Dumping {d}x{d} RGBA texture data", .{ texture.width, texture.height });
+
+            var out_filename_with_extension = out_filename;
+            const out_file = createOutFile(allocator, &out_filename_with_extension, "rgba") catch {
+                std.log.err("Failed to open {s}", .{out_filename_with_extension});
+                printUsage(true);
+                return;
+            };
+            defer out_file.close();
+
+            var buffered_writer = std.io.bufferedWriter(out_file.writer());
+            const writer = buffered_writer.writer();
+
+            writer.writeAll(texture.mips[0]) catch {
+                std.log.err("Error writing output file", .{});
+                return;
+            };
+            try buffered_writer.flush();
+        },
+    }
+}
+
+fn printUsage(comptime following_error: bool) void {
+    const reset = "\x1b[0m";
+    const bold = "\x1b[1m";
+    const underline = "\x1b[4m";
+
+    print(if (following_error)
+        \\
+        \\Usage: {s}{s}{s} [{s}XNB file{s}] [{s}output file{s}]
+    else
+        \\Read content from an XNB file into an output file (extension will be added automatically):
+        \\
+        \\    {s}{s}{s} [{s}XNB file{s}] [{s}output file{s}]
+        \\
+        \\Omitting or writing "-" as either of the filenames will read from stdin or write to stdout respectively.
+    , .{
+        bold,
+        program_name,
+        reset,
+        underline,
+        reset,
+        underline,
+        reset,
+    });
+}
+
+// Copied from std.log.defaultLog
+fn print(comptime format: []const u8, args: anytype) void {
+    const stderr = std.io.getStdErr().writer();
+    var bw = std.io.bufferedWriter(stderr);
+    const writer = bw.writer();
+
+    std.debug.lockStdErr();
+    defer std.debug.unlockStdErr();
+    nosuspend {
+        writer.print(format ++ "\n", args) catch return;
+        bw.flush() catch return;
+    }
+}
+
+fn createOutFile(allocator: std.mem.Allocator, filename: *[:0]const u8, extension: []const u8) !std.fs.File {
+    if (std.mem.eql(u8, filename.*, "-")) {
+        return std.io.getStdOut();
+    }
+
+    filename.* = try std.fmt.allocPrintZ(allocator, "{s}.{s}", .{ filename.*, extension });
+
+    return std.fs.cwd().createFile(filename.*, .{});
+}
---
+++ src/root.zig
@@ -0,0 +1,209 @@
+const std = @import("std");
+
+pub const Effect = struct {
+    // TODO
+    fn parse(allocator: std.mem.Allocator, reader: std.io.AnyReader) !@This() {
+        _ = allocator;
+        _ = reader;
+        return .{};
+    }
+};
+
+pub const PixelFormat = enum(u32) {
+    color,
+    bgr565,
+    bgra5551,
+    bgra4444,
+    dxt1,
+    dxt3,
+    dxt5,
+    normalized_byte2,
+    normalized_byte4,
+    rgba1010102,
+    rg32,
+    rgba64,
+    alpha8,
+    single,
+    vector2,
+    vector4,
+    half_single,
+    half_vector2,
+    half_vector4,
+    hdr_blendable,
+};
+
+pub const Texture2D = struct {
+    pixel_format: ?PixelFormat,
+    width: u32,
+    height: u32,
+    mips: [][]u8,
+
+    fn parse(allocator: std.mem.Allocator, reader: std.io.AnyReader) !@This() {
+        var texture: @This() = undefined;
+
+        texture.pixel_format = reader.readEnum(PixelFormat, .little) catch |err| switch (err) {
+            // 0.15.0
+            // error.InvalidEnumTag => null,
+            error.InvalidValue => null,
+            else => return err,
+        };
+        texture.width = try reader.readInt(u32, .little);
+        texture.height = try reader.readInt(u32, .little);
+
+        if (texture.width == 0 or texture.height == 0) {
+            return XnbParseError.InvalidTexture2DSize;
+        }
+
+        const mip_count = try reader.readInt(u32, .little);
+        texture.mips = try allocator.alloc([]u8, mip_count);
+
+        for (texture.mips) |*mip| {
+            const size = try reader.readInt(u32, .little);
+            mip.* = try allocator.alloc(u8, size);
+            try reader.readNoEof(mip.*);
+        }
+
+        return texture;
+    }
+};
+
+pub const XnbParseError = error{
+    CompressedUnsupported,
+    InvalidMagic,
+    InvalidTexture2DSize,
+    InvalidTypeReaderIndex,
+    SharedResourcesUnsupported,
+    UnknownTypeReader,
+};
+
+pub const Platform = enum {
+    windows,
+    windows_phone,
+    xbox_360,
+};
+
+pub const Xnb = struct {
+    /// Supported platforms for this resource.
+    platform: ?Platform,
+    /// Version number that determines compatibility with releases of XNA Game Studio.
+    version: u8,
+    /// Raw flags. (TODO: parse)
+    flags: u8,
+    /// Size of the file.
+    size: u32,
+    /// Metadata of readers used to parse the content of the file.
+    type_readers: []TypeReader,
+    /// The resource contained in the file.
+    resource: ResourceType,
+
+    pub const TypeReader = struct {
+        name: []u8,
+        version: i32,
+    };
+
+    pub const ResourceType = union(enum) {
+        effect: Effect,
+        texture_2d: Texture2D,
+    };
+
+    /// Parse an XNB file from a reader.
+    pub fn parse(allocator: std.mem.Allocator, reader: std.io.AnyReader) !@This() {
+        var xnb: @This() = undefined;
+
+        try xnb.parseHeader(allocator, reader);
+        try xnb.parseResource(allocator, reader);
+
+        // TODO check that size is correct
+
+        return xnb;
+    }
+
+    fn parseHeader(self: *@This(), allocator: std.mem.Allocator, reader: std.io.AnyReader) !void {
+        // Handle osu!stable special case where an empty header can indicate
+        // that this XNB file contains a single Texture2D
+        // TODO: (13 zeroes followed by Texture2D)
+
+        if (!try reader.isBytes("XNB")) {
+            return XnbParseError.InvalidMagic;
+        }
+
+        self.platform = switch (try reader.readByte()) {
+            'w' => .windows,
+            'm' => .windows_phone,
+            'x' => .xbox_360,
+            else => null,
+        };
+        self.version = try reader.readByte();
+        self.flags = try reader.readByte();
+
+        if (self.flags & 0x80 != 0) {
+            return XnbParseError.CompressedUnsupported;
+        }
+
+        self.size = try reader.readInt(u32, .little);
+
+        const type_reader_count = try std.leb.readUleb128(u32, reader);
+        self.type_readers = try allocator.alloc(TypeReader, type_reader_count);
+
+        for (self.type_readers) |*type_reader| {
+            type_reader.name = try readString(allocator, reader);
+            type_reader.version = try reader.readInt(i32, .little);
+
+            blk: {
+                for (all_type_readers) |type_reader_name| {
+                    if (std.mem.eql(u8, type_reader.name, type_reader_name)) {
+                        break :blk;
+                    }
+                }
+                return XnbParseError.UnknownTypeReader;
+            }
+        }
+
+        if (try std.leb.readUleb128(u32, reader) != 0) {
+            return XnbParseError.SharedResourcesUnsupported;
+        }
+    }
+
+    const all_type_readers = &[_][]const u8{
+        "Microsoft.Xna.Framework.Content.EffectReader",
+        "Microsoft.Xna.Framework.Content.Texture2DReader",
+    };
+
+    fn parseResource(self: *@This(), allocator: std.mem.Allocator, reader: std.io.AnyReader) !void {
+        var type_reader_index = try std.leb.readUleb128(u32, reader);
+
+        if (type_reader_index == 0) {
+            return;
+        }
+
+        type_reader_index -= 1;
+
+        if (type_reader_index >= self.type_readers.len) {
+            return XnbParseError.InvalidTypeReaderIndex;
+        }
+
+        const type_reader = self.type_readers[type_reader_index];
+        const all_type_readers_index = blk: {
+            for (all_type_readers, 0..) |type_reader_name, index| {
+                if (std.mem.eql(u8, type_reader.name, type_reader_name)) {
+                    break :blk index;
+                }
+            }
+            unreachable;
+        };
+
+        self.resource = switch (all_type_readers_index) {
+            0 => .{ .effect = try Effect.parse(allocator, reader) },
+            1 => .{ .texture_2d = try Texture2D.parse(allocator, reader) },
+            else => unreachable,
+        };
+    }
+};
+
+/// Read a string with its length indicated by a LEB128.
+fn readString(allocator: std.mem.Allocator, reader: std.io.AnyReader) ![]u8 {
+    const length = try std.leb.readUleb128(u32, reader);
+    const buffer = try allocator.alloc(u8, length);
+    try reader.readNoEof(buffer);
+    return buffer;
+}
